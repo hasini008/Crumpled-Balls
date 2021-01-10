4b08cd211b466332d8a07ff1137afa33185874e3
hasini008 <74893561+hasini008@users.noreply.github.com>
2021-01-10 12:58:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,8 +16,6 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-
-	//Create the Bodies Here.
     
 	paper = new Shelf();
 	
@@ -25,6 +23,27 @@ function setup() {
 
     ground = new Ground();
 
+boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
 	Engine.run(engine);
   
